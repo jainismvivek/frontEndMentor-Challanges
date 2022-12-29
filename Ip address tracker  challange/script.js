2faad8915b8faac9c446cd2bd2a5ea49ecc08c0b
jainismvivek <vivek.jainism@gmail.com>
2022-12-29 19:07:33 +0530

    initial

---
+++ Ip address tracker  challange/script.js
@@ -0,0 +1,10 @@
+
+const inputIP = document.querySelector(".app__input--ip");
+const buttonSubmit = document.querySelector(".btn");
+const userIP = document.querySelector(".user-ip");
+const userLocation = document.querySelector(".user-location");
+const userTimezone = document.querySelector(".user-timezone");
+const userServiceName = document.querySelector(".user-service-name");
+const userServiceValue = document.querySelector(".user-service-value");
+const modal = document.querySelector(".modal");
+const svg = document.querySelector(".o");
